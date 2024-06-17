46bb236d17e9265bc7113277da941e548cefe72d
skipglide <154386434+skipglide@users.noreply.github.com>
2024-06-17 16:53:33 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from vsa.vector import CleanUpMemory, generate_symbol, generate_key, simularity
+from vsa.vector import CleanUpMemory, permute_forward, permute_inverse, generate_permutation, generate_symbol, generate_key, simularity
 
 d = 1000
 
 entries = CleanUpMemory(d)
 permutations = CleanUpMemory(d)
 links = CleanUpMemory(d)
+
+a = generate_symbol(1, d)
+b = generate_symbol(1, d)
+p = generate_permutation(d)
