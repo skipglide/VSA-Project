d0fc4d7c9644dd6d2f4e42b174726ce84dee5b07
skipglide <154386434+skipglide@users.noreply.github.com>
2024-06-20 17:07:06 -0500

    added features

--- vsa/token.py
+++ vsa/token.py
@@ -1,4 +1,5 @@
-from jax.numpy import ones
+from numpy import ones
+from vector import bind
 
 def get_unicode_number(char):
   """
@@ -9,20 +10,28 @@ def get_unicode_number(char):
 
 class Scanner:
   def __init__(self, symbol_library):
-    self.symbolic_memory_length = 20
     self.symbol_library = symbol_library
-    self.token_memory = [ones(symbol_library.dimensionality, dtype=float16) for _ in range(2)]
-    self.symbolic_memory = []
+
+    self.token_window = 2
+    self.symbolic_memory_length = 10
+    
+    self.token_memory = [ones(symbol_library.dimensionality, dtype=float16) for _ in range(self.token_window)]
+    self.symbolic_memory = [ones(symbol_library.dimensionality, dtype=float16) for _ in range(self.symbolic_memory_length)]
+  
+  def update_token_memory(self, t):
+    symbol = self.symbol_library.retrieve_value(t)
+    self.token_memory[1] = self.token_memory[0]
+    self.token_memory[0] = symbol
 
   def read_token(self, char):
+    # FIX THAT LOGIC
     t = get_unicode_number(char)
     if self.symbol_library.already_there(t):
-      symbol = self.symbol_library.retrieve_value(t)
-      self.token_memory.pop[0]
-      self.token_memory.extend(symbol)
-      return symbol
+      self.update_token_memory(t)
     else:
       self.symbol_library.add_key(t)
+      self.update_token_memory(t)
 
-  def update_memory(self, t):
-    self.token_memory[0] = (,)
+  def update_memory(self):
+    for i in range(self.symbolic_memory_length):
+      new_symbol = bind(self.token_memory[0], )
