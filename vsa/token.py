a90c0f4794eb3e67a20cf220c31bcda68a78856c
hopskip <hopskipone@gmail.com>
2024-06-21 17:27:18 -0500

    Update token.py

--- vsa/token.py
+++ vsa/token.py
@@ -13,11 +13,9 @@ class Scanner:
     self.symbol_library = symbol_library
     self.pattern_cleanup = cleanup_memory
 
-    self.token_window = 2
-    self.symbolic_memory_length = 10
-    
-    self.token_memory = [ones(symbol_library.dimensionality, dtype=float16) for _ in range(self.token_window)]
-    self.symbolic_memory = [ones(symbol_library.dimensionality, dtype=float16) for _ in range(self.symbolic_memory_length)]
+    self.token_window = 10
+    self.token_memory = []
+    self.symbol_memory = [ones(symbol_library.dimensionality, dtype=float16) for _ in range(self.token_window)]
 
     self.pattern_count = dict()
 
@@ -25,16 +23,14 @@ class Scanner:
     key = array_to_tuple(symbol)
     self.pattern_count[key] = pattern_counts.get(key, 0) + 1
 
-  def update_token_memory(self, t):
-    symbol = self.symbol_library.retrieve_value(t)
-    self.token_memory[1] = self.token_memory[0]
-    self.token_memory[0] = symbol
+  def update_token_memory(self, symbol):
+    self.token_memory.insert(symbol)
+    self.token_memory.pop(-1)
 
   def read_token(self, char):
-    # FIX THAT LOGIC
     t = get_unicode_number(char)
     if self.symbol_library.already_there(t): # If it's already known, just update the memory and move.
-      self.update_token_memory(t)
+      self.update_token_memory
     else: # If it's new, add it to the symbol library and the cleanup memory too.
       self.symbol_library.add_key(t)
       symbol = self.symbol_library.retrieve_symbol(t)
@@ -45,11 +41,12 @@ class Scanner:
     self.update_pattern_count(symbol)
 
   def update_memory(self):
-    for i in range(self.symbolic_memory_length):
-      new_symbol # TODO: oops we need to use permutations
-      self.pattern_cleanup.memory.add(new_symbol)
-      self.symbolic_memory[i] = new_symbol
-      self.update_pattern_count(new_symbol)
+    for i in range(self.token_window - 1):
+      strings = self.token_memory[0:i]
+      # Okay, now it needs to take that list of char and encode them into symbols
+      # Okay, now that you have those symbols, now you need to turn them into a permutation sequence 
+      # Cool, with that permutation sequence, we can now record its occurance in the update_pattern_count
+      # Uhhh, okay I think that's it?
 # Opps
 # We forgot to incorporate a way to effiently pair symbols with the original pattern
 # Gonna have to rewrite
