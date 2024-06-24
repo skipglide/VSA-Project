ec60e6ad50427b178820d427fbd281559a9fd09b
skipglide <gravesdavid133@gmail.com>
2024-06-23 21:10:41 -0500

    added test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+from vsa import CleanUpMemory, LookUpMemory, SymbolLibrary
+import vsa as v
+
+dimensionality = 1000
+
+cleanup = CleanUpMemory(dimensionality)
+lookup = LookUpMemory(dimensionality)
+symbol_library = SymbolLibrary(dimensionality)
+
