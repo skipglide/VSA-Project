2d7475d0cae9b39b7c90fa09c6ee4111a59ee6af
skipglide <gravesdavid133@gmail.com>
2024-06-23 21:10:49 -0500

    fixed bugs

--- vsa/vector.py
+++ vsa/vector.py
@@ -3,6 +3,10 @@ import numpy as np
 def link(a, b):
   pass
 
+def sort(unsorted_list):
+  sorted_list = sorted(unsorted_list, key=lambda x: x[0])
+  return sorted_list[0][-1]
+
 def bundle(*symbols):
     symbols = np.stack(symbols, axis=0)
     #convert each angle to a complex number
@@ -21,11 +25,11 @@ def bundle(*symbols):
 def sequence(a, b):
   pass
 
-def permute_forward(x, p):
-  return np.dot(p, x)
+def permute_forward(x, P):
+  return np.dot(x, P)
 
-def permute_inverse(x, p):
-  return np.dot(p.T, x)
+def permute_inverse(x, P):
+  return np.dot(x, P.T)
 
 def generate_permutation(n):
     # Create an identity matrix
@@ -38,22 +42,24 @@ def generate_permutation(n):
     return identity[perm]
 
 def generate_symbol(dimensionality: int):
-  return np.random.uniform(minval=-1.0, maxval=1.0, shape=(1, dimensionality))
+  symbol = np.random.uniform(low=-1.0, high=1.0, size=(1, dimensionality))
+  return symbol
 
-def array_to_tuple(arr):
-    return tuple(arr)
+def array_to_bytes(arr):
+    return arr.tobytes()
 
-def tuple_to_array(tup):
-    return np.array(tup, dtype=np.float32)
+def bytes_to_array(bites):
+    array = np.frombuffer(bites, dtype=np.float64)
+    return array
 
 def similarity(a,b):
     assert a.shape[-1] == b.shape[-1], "VSA Dimension must match: " + str(a.shape) + " " + str(b.shape)
     #multiply values by pi to move from (-1, 1) to (-π, π)
     pi = np.pi
-    a = jnp.multiply(a, pi)
-    b = jnp.multiply(b, pi)
+    a = np.multiply(a, pi)
+    b = np.multiply(b, pi)
     #calculate the mean cosine similarity between the vectors
-    similarity = np.mean(np.cos(a - b), axis=1)
+    similarity = np.mean(np.cos(a - b), axis=-1)
     return similarity
 
 class SymbolLibrary:
@@ -79,24 +85,34 @@ class LookUpMemory:
     self.dimensionality = d
 
   def add_symbol(self, a):
+    '''
+    Takes thing 'a' and generates a symbol to return
+    '''
     x = generate_symbol(self.dimensionality)
-    self.memory[array_to_tuple(x)] = a
+    self.memory[array_to_bytes(x)] = a
     return x
 
   def add_association(self, x, a):
-    self.memory[x] = a
+    """
+    Takes a symbol 'x' and uses it as key for retreiving 'a'
+    """
+
+    self.memory[array_to_bytes(x)] = a
   
   def return_simularity(self, x):
+    """
+    Takes the symbol 'x' and returns a list of
+    """
     # This is a naive implimentation until I incorporate annoy
     result = []
     for association in self.memory:
-      a = tuple_to_array(association[0])
-      result.append((simularity(x, a), a))
+      a = bytes_to_array(association)
+      result.append((similarity(x, a), a))
     return result
   
   def retrieve_value(self, x):
       try:
-          return self.memory[x]
+          return self.memory[array_to_bytes(x)]
       except KeyError:
           print("No value")
           return None
@@ -108,28 +124,28 @@ class CleanUpMemory:
   
   def add(self, *args):
     for x in args:
-      self.memory.add(array_to_tuple(x))
+      self.memory.add(array_to_bytes(x))
   
   def check_memory(self, x):
     """
     Check if x is already in clean up memory.
     """
-    if array_to_tuple(x) in self.memory:
+    if array_to_bytes(x) in self.memory:
       return True
     else:
       return False
   
   def clean_up(self, x):
-    list = self.return_simularity(x)
-    sorted_list = sorted(list, key=lambda x: x[0])
+    unsorted_list = self.return_simularity(x)
+    sorted_list = sorted(unsorted_list, key=lambda x: x[0])
     return sorted_list[0][-1]
 
   def return_simularity(self, x):
     # This is a naive implimentation until I incorporate annoy
     result = []
     for symbol in self.memory:
-      a = tuple_to_jax_array(symbol)
-      result.append((simularity(x, a), a))
+      a = bytes_to_array(symbol)
+      result.append((similarity(x, a), a))
     return result
 
 class FHRR:
