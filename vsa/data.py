cd8bd7b09e9c5b68ef15691bdcee0907abdfc71f
skipglide <154386434+skipglide@users.noreply.github.com>
2024-06-26 16:11:02 -0500

    Added stuff

---
+++ vsa/data.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class FHRR:
+    name: str
+    dept: str
+    salary: int
--- vsa/utils.py
+++ vsa/utils.py
@@ -1,6 +1,28 @@
 import time
 from typing import Any, Callable, Optional
 
+#https://claude.ai/chat/c925cd4d-c149-4526-ade7-8a947a589c4a
+def timer_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        try:
+            result = func(*args, **kwargs)
+        finally:
+            end_time = time.perf_counter()
+            print(f"{func.__name__} execution time: {end_time - start_time:.6f} seconds")
+        return result
+    return wrapper
+
+class CountCalls:
+    def __init__(self, func):
+        self.func = func
+        self.num_calls = 0
+
+    def __call__(self, *args, **kwargs):
+        self.num_calls += 1
+        print(f"Call {self.num_calls} of {self.func.__name__}")
+        return self.func(*args, **kwargs)
 
 class CallableResult:
 
