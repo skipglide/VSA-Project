14879012af5c3ac32cb3bf714a4df45d6aee093c
skipglide <154386434+skipglide@users.noreply.github.com>
2024-07-01 13:46:45 -0500

    renamed functions

--- vsa/token.py
+++ vsa/token.py
@@ -1,4 +1,4 @@
-from numpy import ones, float16
+from numpy import zeros, float16
 from .vector import generate_symbol
 from .connect import generate_permutation, \
   permute_forward, permute_inverse
@@ -20,9 +20,9 @@ class Scanner:
     self.dimensionality = symbol_library.dimensionality
     # TODO: add check that all these memories are the same dimensionality
 
-    self.token_window = 10 # length of token list
-    self.token_memory = [] # contains 
-    self.symbol_memory = [ones(symbol_library.dimensionality, dtype=float16) for _ in range(self.token_window)]
+    self.token_window = 10 # length of token memory
+    self.token_memory = [] # I store chars
+    self.symbol_memory = [zeros(symbol_library.dimensionality, dtype=float16) for _ in range(self.token_window)] # I store the corresponding symbols that represent each char
 
     self.pattern_count = dict()
 
@@ -31,7 +31,10 @@ class Scanner:
     self.permutation_lookup.add_association(p, perm)
     return p
 
-  def array_to_sequence(self, arr):
+  def arrays_to_sequence(self, arr):
+    """
+    Takes a list of symbols and turns them into a VSA sequence representation.
+    """
     perm = generate_permutation(self.dimensionality)
     s = arr[0] # The begining of the sequence
     for symbol in arr[1:-1]: # Leave out the first element
@@ -44,8 +47,8 @@ class Scanner:
   def update_pattern_count(self, string):
     self.pattern_count[string] = self.pattern_count.get(string, 0) + 1
 
-  def update_token_memory(self, symbol):
-    self.token_memory.insert(0, symbol) # First in line
+  def update_token_memory(self, char): # A simple procedure, 
+    self.token_memory.insert(0, char) # First in line
     self.token_memory.pop(-1) # Get rid of last entry
 
   def read_token(self, char):
@@ -53,25 +56,24 @@ class Scanner:
     if self.symbol_library.already_there(t): # If it's already known, just update the memory and move.
       self.update_token_memory
     else: # If it's new, add it to the symbol library and the cleanup memory too.
-      self.symbol_library.add_key(t)
-      symbol = self.symbol_library.retrieve_symbol(t)
-      self.pattern_cleanup.add(symbol)
-      self.update_token_memory(t)
-    # I guess we just update the memory?
-    self.update_memory()
+      self.symbol_library.add_key(t) # Adding a key generates a symbol for you.
+      symbol = self.symbol_library.retrieve_symbol(t) # And you can retreive it with the key we just used.
+      self.pattern_cleanup.add(symbol) # Add it to the cleanup memory because it's new.
+      self.update_token_memory(char) # And update the token memory too.
+    self.update_model() # Finish it
     pass
 
   def get_symbol_with_char(self, char):
     return self.symbol_library.retrieve_symbol(get_unicode_number(char))
 
-  def update_memory(self):
-    # You can assume that any unicode number has a corresponding symbol in 
-    for i in range(self.token_window - 1):
+  def update_model(self):
+    # You can assume that any unicode number has a corresponding symbol in symbol_library
+    for i in range(self.token_window - 1): # Scan for patterns of a certain size from 
       char_list = self.token_memory[0:i]
       # Okay, now it needs to take that list of char and encode them into symbols
       symbols = [self.get_symbol_with_char(char) for char in char_list]
       # Okay, now that you have those symbols, now you need to turn them into a permutation sequence
-      sequence_symbol = self.array_to_sequence(symbols)
+      sequence_symbol = self.arrays_to_sequence(symbols)
       # Cool, with that permutation sequence, we can now record its occurance in the update_pattern_count
       string = ''.join(char_list)
       self.update_pattern_count(string)
